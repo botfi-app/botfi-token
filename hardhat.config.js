e72d4aa9014d8ac701d3a5e1f6ce44590fc55b7a
Bot Papi <73213378+big-zak@users.noreply.github.com>
2023-11-18 23:47:35 +0000

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,37 @@
+const {
+  etherscanApi,
+  accounts
+} = require("./secrets");
+
 require("@nomicfoundation/hardhat-toolbox");
+
+require("hardhat-gas-reporter");
+
+require("@nomicfoundation/hardhat-verify");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+
+  networks: {
+
+    bsc_testnet: {
+      url: `https://data-seed-prebsc-2-s1.binance.org:8545/`,
+      chainId: 1,
+      accounts
+    }
+  },
+
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: etherscanApi
+  },
+
+  sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    enabled: true
+  },
+
   solidity: "0.8.19",
 };
